56e1f4e0ed99b389cb58051515bea0e5eb67a94e
adityas0604 <dharmaa130@gmail.com>
2026-02-09 03:02:53 -0600

    fixed error in seed file

--- backend/shared/seeddb.js
+++ backend/shared/seeddb.js
@@ -1,6 +1,7 @@
 // seed the database wiht the stock data of last seven trading days
 
 import { ingestWinner } from '../cron/utils/ingestWinner.js';
+import { getLastNTradingDays } from './helpers.js';
 
 const ONE_DAY_MS = 24 * 60 * 60 * 1000;
 
