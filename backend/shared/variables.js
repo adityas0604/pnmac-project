b8b368d16f9f81a1721b2ee1014ee0acb9b4f236
adityas0604 <dharmaa130@gmail.com>
2026-02-10 00:45:03 -0600

    added name as env variable

--- backend/shared/variables.js
+++ backend/shared/variables.js
@@ -5,5 +5,5 @@ const secrets = await loadSecrets();
 export const WATCHLIST = ["AAPL", "MSFT", "GOOGL", "AMZN", "TSLA", "NVDA"];
 export const WINNER_TABLE = 'ticker-winner';
 
-export const MASSIVE_API_KEY = secrets.MASSIVE_API_KEY;
+export const MASSIVE_API_KEY = secrets[process.env.MASSIVE_API_KEY_NAME] ;
 
